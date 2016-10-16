9d0117301d981990615f3b276d37012874564e34
Danni <Danni@192.168.1.64>
2016-10-16 01:04:31 -0500

    Libreria de Conexión con base de datos. Revisar ejemplo.

---
+++ src/controls/LibreriaConexionEjemplo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controls;
+
+/**
+ *
+ * @author Danni
+ */
+public class LibreriaConexionEjemplo {
+    /*
+package prueba.bd;
+import jdda.ConnectionDB; NOMBRE DE LA LIBRERÍA
+
+public class PruebaBD {
+    
+    public static void main(String[] args) {
+        ConnectionDB con = new ConnectionDB(); 
+        con.Connection("prueba", "root", ""); METODO CONEXION RECIBE PARAMETROS: TABLA,USUARIO,CONTRASEÑA
+        con.basicConsult("7", "id", "prueba"); METODO CONSULTA RECIBE PARAETROS: DATO, CAMPO, TABLA. DINÁICO
+    //SE ACCEDE A LA INFO DE LA CONSULTA MEDIANTE LOS INDICES DE "getColumn()[indice]".
+    //Introducir el numero de columnas en el bucle para traer todo. 
+        for(int i=0;i<6;i++){
+            System.out.println(con.getColumn()[i]);
+        }
+    }
+    */
+}
+    
--- src/views/ViewMain.java
+++ src/views/ViewMain.java
@@ -27,7 +27,24 @@ public class ViewMain extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jMenu1.setText("Buscar");
+
+        jMenuItem1.setText("jMenuItem1");
+        jMenu1.add(jMenuItem1);
+
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Agregar");
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -37,7 +54,7 @@ public class ViewMain extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 279, Short.MAX_VALUE)
         );
 
         pack();
@@ -79,5 +96,9 @@ public class ViewMain extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
     // End of variables declaration//GEN-END:variables
 }
