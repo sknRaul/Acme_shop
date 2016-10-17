fdf850a954a385206ff269a817ca36b8d526c2da
panda <panda@panda-pc>
2016-10-16 19:15:48 -0500

    Start ControlSuppliers

--- src/controls/ControlSuppliers.java
+++ src/controls/ControlSuppliers.java
@@ -4,11 +4,36 @@
  * and open the template in the editor.
  */
 package controls;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import views.ViewSuppliers;
+import models.ModelSuppliers;
 /**
  *
  * @author panda
  */
-public class ControlSuppliers {
+public class ControlSuppliers implements ActionListener {
+
+    private ModelSuppliers modelSuppliers;
+    private ViewSuppliers viewSuppliers;
+    
+    public ControlSuppliers(ModelSuppliers modelSuppliers, ViewSuppliers viewSuppliers){
+        this.modelSuppliers = modelSuppliers;
+        this.viewSuppliers = viewSuppliers;
+        
+        inView();
+        
+    }
+    
+    public void inView(){
+        this.viewSuppliers.setTitle("Proveedores");
+        this.viewSuppliers.setVisible(true);
+        this.viewSuppliers.setLocationRelativeTo(null);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
